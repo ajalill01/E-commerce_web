44414e4d27cd4b9d4dd24351848ae8df6f8c4dc6
ajalil01 <ahmed.benzid@etu.univ-batna2.dz>
2025-06-01 02:36:52 -0700

    addin trust proxy

--- app.js
+++ app.js
@@ -1,59 +1,44 @@
-require("dotenv").config();
-
-const express= require("express");
-const rateLimit = require('express-rate-limit');
-const cors = require('cors')
-const sanitize = require('./backend/middleware/sanitize'); 
-const helmet = require('helmet');
 
 
 // const dotenv = require('dotenv');
 // const createAdmin = require('./backend/controllers/add-admin');
 
+// createAdmin();
+
+require("dotenv").config();
+const express = require("express");
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
+const sanitize = require('./backend/middleware/sanitize'); 
+const helmet = require('helmet');
+
 const app = express();
 
+
 app.use(helmet());
-
-
-const authRoutes = require('./backend/routes/auth-route');
-const brandRoutes = require('./backend/routes/brand-route');
-const carRoutes = require('./backend/routes/car-route');
-const productRoutes = require('./backend/routes/product-route');
-const orderRoutes = require('./backend/routes/orders-route');
-const statsRoute = require("./backend/routes/statsRoute");
-
-
-
-const consectDB=require("./backend/database/db.js"); 
-app.use(cors())
-app.use(express.json());
-
+app.set('trust proxy', 1);
+app.use(express.json()); 
+app.use(cors());
 app.use(sanitize);
 
 
-
 const globalLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 100,
-    message: 'Too many requests, please try again later.',
-  });
-  
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: 'Too many requests, please try again later.',
+  validate: { trustProxy: true } 
+});
 app.use(globalLimiter);
 
-app.use('/api/auth',authRoutes);
-app.use('/api/brands', brandRoutes);
-app.use('/api/cars', carRoutes);
-app.use('/api/products', productRoutes);
-app.use('/api/orders', orderRoutes);
-app.use("/api/stats", statsRoute);
+
+app.use('/api/auth', require('./backend/routes/auth-route'));
+app.use('/api/brands', require('./backend/routes/brand-route'));
+app.use('/api/cars', require('./backend/routes/car-route'));
+app.use('/api/products', require('./backend/routes/product-route'));
+app.use('/api/orders', require('./backend/routes/orders-route'));
+app.use("/api/stats", require("./backend/routes/statsRoute"));
 
 
-const PORT= process.env.PORT||3000;
-
-
-consectDB();
-app.listen(PORT,()=>{
-    console.log("server is on");
-});
-
-// createAdmin();
+const PORT = process.env.PORT || 3000;
+require("./backend/database/db.js")(); 
+app.listen(PORT, () => console.log("Server is running on port", PORT));
